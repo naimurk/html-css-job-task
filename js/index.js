00bec09f8b8972d03cdefbe3ea128e9067ed7339
naimurk <naimurk781@gmail.com>
2024-10-04 21:22:49 +0600

    create modal using javscript

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const menuBtn = document.getElementById("menuBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden");
-});
+// const menuBtn = document.getElementById("menuBtn");
+// const mobileMenu = document.getElementById("mobileMenu");
+// menuBtn.addEventListener("click", () => {
+//   mobileMenu.classList.toggle("hidden");
+// });
 
 
 // document.addEventListener("scroll", function () {
@@ -15,4 +15,53 @@ menuBtn.addEventListener("click", () => {
 //       navbar.classList.add("navbar-default");
 //     }
 //   });
+
+
+// const menuBtn = document.getElementById('menuBtn');
+// const mobileMenu = document.getElementById('mobileMenu');
+
+// menuBtn.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('hidden');
+// });
+
+// function openModal(title, description) {
+//   const modal = document.getElementById('modal');
+//   const modalTitle = document.getElementById('modal-title');
+//   const modalDescription = document.getElementById('modal-description');
+
+//   modalTitle.textContent = title;
+//   modalDescription.textContent = description;
+//   modal.classList.remove('hidden');
+// }
+
+// function closeModal() {
+//   const modal = document.getElementById('modal');
+//   modal.classList.add('hidden');
+// }
   
+function openModal(title, content) {
+    document.getElementById("modal").classList.remove("hidden");
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalContent").innerText = content;
+  }
+
+  // Function to close the modal
+  function closeModal() {
+    document.getElementById("modal").classList.add("hidden");
+  }
+
+  // Function to open the quote modal
+  function openQuoteModal() {
+    document.getElementById("quoteModal").classList.remove("hidden");
+  }
+
+  // Function to close the quote modal
+  function closeQuoteModal() {
+    document.getElementById("quoteModal").classList.add("hidden");
+  }
+
+  // Mobile menu toggle
+  document.getElementById("menuBtn").addEventListener("click", function () {
+    const mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.classList.toggle("hidden");
+  });
